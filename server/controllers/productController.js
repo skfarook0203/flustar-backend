ef0c89b03ae11b85f9965a8abf857937f7745a83
Shaik Farook <skfarook0203@gmail.com>
2026-01-04 22:05:44 +0530

    Create productController.js

---
+++ server/controllers/productController.js
@@ -0,0 +1,22 @@
+const Product = require('../models/Product');
+
+// Get all products
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Get product by ID
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ message: 'Product not found' });
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
